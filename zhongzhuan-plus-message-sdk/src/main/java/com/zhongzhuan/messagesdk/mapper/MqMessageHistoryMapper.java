76d15a711fcd85980aa4e290f56dc1a249909053
gogs <57864074+Karat-Hujiajun@users.noreply.github.com>
2024-03-20 10:19:21 +0800

    Day9课程发布流程完成

---
+++ zhongzhuan-plus-message-sdk/src/main/java/com/zhongzhuan/messagesdk/mapper/MqMessageHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.zhongzhuan.messagesdk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhongzhuan.messagesdk.model.po.MqMessageHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageHistoryMapper extends BaseMapper<MqMessageHistory> {
+
+}
